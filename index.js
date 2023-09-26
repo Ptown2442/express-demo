46a3c354f35ffc161473ce21feae7931b975ec24
Ptown2442 <portervaughn93@gmail.com>
2023-09-25 22:39:31 -0700

    mongodb initial

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-console.log('before');
-getRepositories('Porter', callback);
-console.log('after');
+const mongoose = require('mongoose');
 
-
-
-function getRepositories(username, callback) {
-    const repos =  ['repo1', 'repo2', 'repo3'];
-    setTimeout(() => {
-        callback(repos);
-    }, 2000);
-}
-
-function callback(repos) {
-    for (let i in repos) console.log(`repo ${i} : ${repos[i]}`)
-};
+mongoose.connect('mongodb://localhost')
